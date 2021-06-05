9dc843474812d4d7052d7e47799548bada40a842
Tushar <64252275+ktushar585@users.noreply.github.com>
2021-06-05 23:35:17 +0530

    Update ode45_a1.m
    Corrected bugs

--- Assignment/Assignment_1/ode45_a1.m
+++ Assignment/Assignment_1/ode45_a1.m
@@ -4,14 +4,12 @@ c1 = 100;
 c2 = 1000;
 k1 = 10000;
 k2 = 100000;
-syms t;
-f = 5*sin(10*t);   %The required force
-fun = @(t,y)[y(3); y(4); (f+k1*y(2)+c1*y(4)-k1*y(1)-c1*y(3))/m1; (k1*y(1)+c1*y(3)-(k1+k2)*y(2)-(c1+c2)*y(4))/m2]
+fun = @(t,y)[y(3); y(4); (3*sin(10*t)+k1*y(2)+c1*y(4)-k1*y(1)-c1*y(3))/m1; (k1*y(1)+c1*y(3)-(k1+k2)*y(2)-(c1+c2)*y(4))/m2]
 tspan = linspace(0, 10, 800);
-[T,Y] = ode45(fun,tspan,[0;0;0;0]);
-plot(T,Y(:,1));
+[t,Y] = ode45(fun,tspan,[0;0;0;0]);
+plot(t,Y(:,1));
 xlabel('Time(s)');ylabel('x1(m)');
 hold on;
-plot(T,Y(:,2));
+plot(t,Y(:,2));
 xlabel('Time(s)');ylabel('x2(m)');
 hold off;
