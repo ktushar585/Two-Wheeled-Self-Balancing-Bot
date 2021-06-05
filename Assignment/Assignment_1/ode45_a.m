3f70edb337e546bd57bdd8d964d39773400fceb2
Tushar <64252275+ktushar585@users.noreply.github.com>
2021-06-05 23:22:12 +0530

    Update ode45_a.m

--- Assignment/Assignment_1/ode45_a.m
+++ Assignment/Assignment_1/ode45_a.m
@@ -7,10 +7,10 @@ k2 = 100000;
 f = 5;   %The required force
 fun = @(t,y)[y(3); y(4); (f+k1*y(2)+c1*y(4)-k1*y(1)-c1*y(3))/m1; (k1*y(1)+c1*y(3)-(k1+k2)*y(2)-(c1+c2)*y(4))/m2]
 tspan = linspace(0, 10, 800);
-[T,Y] = ode45(fun,tspan,[0;0;0;0]);
-plot(T,Y(:,1));
+[t,Y] = ode45(fun,tspan,[0;0;0;0]);
+plot(t,Y(:,1));
 xlabel('Time(s)');ylabel('x1(m)');
 hold on;
-plot(T,Y(:,2));
+plot(t,Y(:,2));
 xlabel('Time(s)');ylabel('x2(m)');
 hold off;
