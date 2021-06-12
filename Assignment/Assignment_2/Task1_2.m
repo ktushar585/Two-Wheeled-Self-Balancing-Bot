e69c38e595d2a336fce360d86a62413a25831812
Tushar <64252275+ktushar585@users.noreply.github.com>
2021-06-12 19:24:40 +0530

    Update Task1_2.m

--- Assignment/Assignment_2/Task1_2.m
+++ Assignment/Assignment_2/Task1_2.m
@@ -1,5 +1,3 @@
-mc = 1.5;
-mp = 0.5;
 g = 9.82;
 L = 1;
 d1 = 0.1;
@@ -13,8 +11,13 @@ B = [0 0 1/mc 1/(L*mc)]';
 C = [1 0 0 0]; %q1 as output
 D = 0;
 sys = ss(A,B,C,D);
-eigs = eig(A)
-pole(sys)
-isstable(sys)
-Q = ctrb(sys); % Returns matrix Q
-rank(Q)
+%eigs = eig(A)
+%pole(sys)
+pzmap(sys)
+grid on
+%isstable(sys)
+%Q = ctrb(sys); % Returns matrix Q
+%rank(Q)
+
+
+% In both the cases of q1 and q2 the system is unstable as it can be seen that we recieve a positive eigenvalue.
