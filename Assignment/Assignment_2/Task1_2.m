745bbee234285f732420df038d3b80ffec8203cf
Tushar <64252275+ktushar585@users.noreply.github.com>
2021-06-12 19:44:49 +0530

    Update Task1_2.m

--- Assignment/Assignment_2/Task1_2.m
+++ Assignment/Assignment_2/Task1_2.m
@@ -1,3 +1,5 @@
+mc = 1.5;
+mp = 0.5;
 g = 9.82;
 L = 1;
 d1 = 0.1;
@@ -7,17 +9,24 @@ A = [0           0               1                 0;
      0       (g*mp)/mc        -d1/mc          -d2/(L*mc);
      0    (g*(mc+mp))/(mc*L)   -d1/(L*mc)    -(d2*(mc+mp))/(L*L*mc*mp)];
 B = [0 0 1/mc 1/(L*mc)]';
-%C = [0 1 0 0]; %q2 as output
-C = [1 0 0 0]; %q1 as output
+C1 = [0 1 0 0]; %q2 as output
+C2 = [1 0 0 0]; %q1 as output
 D = 0;
-sys = ss(A,B,C,D);
+sys1 = ss(A,B,C1,D);
+sys2 = ss(A,B,C2,D);
 %eigs = eig(A)
-%pole(sys)
-pzmap(sys)
+%p1 = pole(sys1)
+%p2 = pole(sys2)
+%z1 = zero(sys1)
+%z2 = zero(sys2)
+pzmap(sys1)
+pzmap(sys2)
 grid on
-%isstable(sys)
-%Q = ctrb(sys); % Returns matrix Q
-%rank(Q)
+%isstable(sys1) #This will be 1 if the system is stable. 
+Q = ctrb(sys1); % Returns matrix Q
+i = rank(Q) %If rank(i) is 4 then the system is controllable.
+R = obsv(sys1);
+j = rank(R) %If rank(j) is 4 then system is observable.
 
 
 % In both the cases of q1 and q2 the system is unstable as it can be seen that we recieve a positive eigenvalue.
